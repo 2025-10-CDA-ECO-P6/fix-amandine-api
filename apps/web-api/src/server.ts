24ccdccce096c0af0bca9aa32303a7d84dc6cfd8
Christophe-008 <christophe@createur-design.fr>
2026-02-19 19:41:44 +0100

    fix: api express ts & docker

--- apps/web-api/src/server.ts
+++ apps/web-api/src/server.ts
@@ -1,12 +1,12 @@
-import express  from "express";
+import express from "express";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/health", (_req, res) => {
-    res.json({ status: "ok" });
+  res.json({ status: "ok" });
 });
 
 app.listen(PORT, () => {
-    console.log("API running on port", PORT);
+  console.log("API running on port", PORT);
 });
